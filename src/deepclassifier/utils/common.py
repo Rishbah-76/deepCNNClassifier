97998a9655fb158dd83f5b02158b8752d8c2e9ea
Rishbah-76 <rbrishabh76@gmail.com>
2022-10-31 17:13:51 +0530

    updated common

--- src/deepclassifier/utils/common.py
+++ src/deepclassifier/utils/common.py
@@ -1,40 +1,38 @@
-from importlib.resources import path
 import os
+from box.exceptions import BoxValueError
 import yaml
+from deepclassifier import logger
 import json
 import joblib
-from box.exceptions import BoxValueError
 from ensure import ensure_annotations
-from deepclassifier import logger
+from box import ConfigBox
 from pathlib import Path
 from typing import Any
-from box import ConfigBox
 
-@ensure_annotations #Act as a Decorator
-def read_yaml(path_to_yaml: Path)->Any:
-    """Read the yaml configuration file and returns
-
-        Args:
-            path_to_yaml: Path to the yaml file.
-        Raises:
-            ValueError: If file is empty
-            e: Empty yaml configuration file
-        Returns:
-            ConfigBox: ConfigBox object Type.
-        
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+    Args:
+        path_to_yaml (str): path like input
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+    Returns:
+        ConfigBox: ConfigBox type
     """
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
-            return ConfigBox(content) 
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
 
+####################################################################################################################    
 
-####################################################################################################################
+
 
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
@@ -48,23 +46,24 @@ def create_directories(path_to_directories: list, verbose=True):
         if verbose:
             logger.info(f"created directory at: {path}")
 
+####################################################################################################################    
 
-####################################################################################################################
+
 
 @ensure_annotations
-def save_json(pathh: Path, data: dict):
+def save_json(path: Path, data: dict):
     """save json data
     Args:
         path (Path): path to json file
         data (dict): data to be saved in json file
     """
-    with open(path,"w") as f:
-        json.dump(data,f,indent=4)
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
 
     logger.info(f"json file saved at: {path}")
 
 @ensure_annotations
-def load_json(path: Path)-> ConfigBox:
+def load_json(path: Path) -> ConfigBox:
     """load json files data
     Args:
         path (Path): path to json file
@@ -72,13 +71,14 @@ def load_json(path: Path)-> ConfigBox:
         ConfigBox: data as class attributes instead of dict
     """
     with open(path) as f:
-        content=json.load(f)
+        content = json.load(f)
 
     logger.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
 
+####################################################################################################################    
 
-####################################################################################################################
+
 
 @ensure_annotations
 def save_bin(data: Any, path: Path):
@@ -104,6 +104,8 @@ def load_bin(path: Path) -> Any:
 
 ####################################################################################################################    
 
+
+
 @ensure_annotations
 def get_size(path: Path) -> str:
     """get size in KB
@@ -114,3 +116,6 @@ def get_size(path: Path) -> str:
     """
     size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
+
+####################################################################################################################    
+
